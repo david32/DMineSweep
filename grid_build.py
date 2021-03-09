ab4b57cff4f7a6682a3c9b395202d7506afee3fc
david32 <david32sy@gmail.com>
2021-03-09 22:13:08 +0100

    Added opening neighboring cells when value is 0 and tested. Added menu Added flaging as mine

--- grid_build.py
+++ grid_build.py
@@ -3,7 +3,7 @@
 import stdio
 import stdarray
 import random
-
+import os
 class mineGrid:
     def __init__(self, row, column):
         self.row = row
@@ -11,6 +11,8 @@ class mineGrid:
         self.PercMine = 0
         
         self.array = stdarray.create2D(row, column , 0)
+        self.front = stdarray.create2D(row, column , "?")
+        stdarray.write2D(self.front)
 
     def createMines(self, percentage):
 
@@ -52,18 +54,87 @@ class mineGrid:
                         self.array[row+1][column + 1] = self.array[row+1][column + 1] + 1
             #stdarray.write2D(self.array)
 
-    def printGrid(self):
+    def printmineGrid(self):
         stdarray.write2D(self.array)
 
+    def printGameGrid(self):
+        stdarray.write2D(self.front)
+
+    def checkBoundries(self, row, column):
+        if row >= 0 and row < self.row and column >= 0 and column < self.column:
+            return True
+        return False
+            
+
+    def getCellValue(self, row, column):
+        if self.checkBoundries(row, column) and self.front[row][column] == "?":
+            return self.array[row][column]
+        
+        
+        return -1
+
+    def openCell(self, row, column):
+        print(" openCell")
+        cellVal = self.getCellValue(row, column)
+        if cellVal == -1 or cellVal == "M":
+            return
+        
+        if cellVal == 'X':
+            print("you lost")
+            self.front = self.array
+            return 400
+
+
+        if cellVal == 0:
+            self.front[row][column] = self.array[row][column]
+            for i in range(row - 1 , row + 2):
+                for j in range(column - 1 , column + 2):
+                    
+                    if not (i == row and j == column): 
+                        self.openCell(i, j)
+        else:
+            self.front[row][column] = self.array[row][column]
+
+    def setMine(self, row, column):
+        print(" setMine")
+        if self.checkBoundries(row, column) and self.front[row][column] == "?":
+            self.front[row][column] = "M"
+
+    def checkCell(self, row, column):
+        print(" checkCell")
+        if self.checkBoundries(row, column) and self.front[row][column] == "?":
+            #something\
+            return
+    def funcSwitcher(self, command, row, column):
+        switcher={
+                1: lambda: self.openCell( row -1, column-1),
+                2: lambda: self.setMine( row -1, column-1),
+                3: lambda: self.checkCell( row -1, column-1),
+                }
+        return switcher.get(command)()
 
 def _main():
     """
     For testing.
     """
+    
     grid = mineGrid(5, 4)
     grid.createMines(20)
 
-    grid.printGrid()
+
+    grid.printmineGrid()
+    print("play, [1/2/3 - open, flag] [row] [columb]: [-1 to exit]")
+    com, row, col = map(int, input().split())
+    while (com != -1):
+        res = grid.funcSwitcher(com, row, col)
+
+        #os.system('cls')
+        grid.printGameGrid()
+        if (res == 400): break
+        
+        
+        com, row, col = map(int, input().split())
+    
 
 if __name__ == '__main__':
     _main()
